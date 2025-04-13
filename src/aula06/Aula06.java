e1cf943856386a89dd740eded94860e756cc939a
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-04-13 19:16:54 -0300

    Curso em Video Java

--- src/aula06/Aula06.java
+++ src/aula06/Aula06.java
@@ -3,10 +3,15 @@ package aula06;
 public class Aula06 {
     public static void main(String[] args){
         ControleRemoto c1 = new ControleRemoto();
-        c1.abrirMenu();
-        c1.fecharMenu();
-        c1.maisVolume(50);
-        c1.abrirMenu();
-        c1.fecharMenu();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.ligar();
+        c1.desligarMudo();
+        c1.play();
     }
 }
--- src/aula06/ControleRemoto.java
+++ src/aula06/ControleRemoto.java
@@ -7,74 +7,116 @@ public class ControleRemoto {
     private boolean tocando;
     private boolean mudo;
 
-    public void Construtor(){
+    public void setVolume(int mixer){
+        this.volume += mixer;
+    }
 
+    public int getVolume(){
+        System.out.println("O volume é igual a " + this.volume + "%");
+        return this.volume;
+    }
+
+    public void setLigado(boolean estado){
+        this.ligado = estado;
+    }
+
+    public void getLigado(){
+        if (this.ligado){
+            System.out.println("O controle está ligado.");
+        }else{
+            System.out.println("O controle está desligado.");
+        }
+    }
+
+    public void setTocando(boolean estado){
+        this.tocando = estado;
+    }
+
+    public void getTocando(){
+        if(this.tocando){
+            System.out.println("O dispositivo está tocando.");
+        }else{
+            System.out.println("O dispositivo não está tocando.");
+        }
     }
 
     public void ligar(){
-        this.ligado = true;
+        this.setLigado(true);
     }
 
     public void desligar(){
-        this.ligado = false;
+        this.setLigado(false);
     }
 
     public void abrirMenu(){
-        System.out.println("O volume está em " + this.volume + "%");
-        if (this.ligado){
-            System.out.println("O controle está ligado");
-        }else{
-            System.out.println("O controle está desligado");
-        }
-        if (this.tocando){
-            System.out.println("O aparelho está desmutado");
-        }else{
-            System.out.println("O aparelho está mutado");
-        }
+        getLigado();
+        getTocando();
+        getVolume();
     }
 
     public void fecharMenu(){
-        System.out.println("O menu foi fechado com sucesso.");
+        System.out.println("O menu está sendo fechado.");
     }
 
-    public void maisVolume(int valor){
-        if (valor < 0) {
-            System.out.println("Valor inválido");
+    public void maisVolume(){
+        if ((this.getVolume() + 10) > 100){
+            System.out.println("O volume está no máximo");
         }else{
-            if ((this.volume + valor) > 100) {
-                this.volume = 100;
-            }else{
-                this.volume += valor;
-            }
+            this.volume += 10;
         }
     }
 
-    public void menosVolume(int valor) {
-        if (valor < 0) {
-            System.out.println("Valor inválido");
-        }else{
-            if ((this.volume - valor) < 0) {
-                this.volume = 0;
-            }else{
-                this.volume -= valor;
-            }
+    public void menosVolume(){
+        if ((this.getVolume() - 10) < 0){
+            System.out.println("O volume está no mínimo");
         }
+    }
+
+    public boolean getMudo(){
+        if (this.mudo) {
+            System.out.println("O volume está mudo");
+            return true;
+        }else{
+            System.out.println("O volume não está mudo");
+            return false;
+        }
+    }
+
+    public void setMudo(boolean valor){
+        this.mudo = valor;
     }
 
     public void ligarMudo(){
-        this.mudo = true;
+        setMudo(true);
     }
 
     public void desligarMudo(){
-        this.mudo = false;
+        setMudo(false);
     }
 
     public void play(){
-        this.tocando = true;
+        if ((this.volume > 0) && (this.ligado) && !(this.mudo)){
+            setTocando(true);
+            System.out.println("O dispositivo está reproduzindo");
+            System.out.println("Volume: ");
+            for (int i = 0 ; i < this.volume ; i+=10){
+                System.out.print("|");
+            }
+        }else{
+            if (this.volume == 0){
+                System.out.println("O volume é igual a 0");
+            }
+            if (!this.ligado){
+                System.out.println("O dispositivo está desligado");
+            }
+            if (this.mudo){
+                System.out.println("O dispositivo está mudo");
+            }
+        }
     }
 
     public void pause(){
-        this.tocando = false;
+        setTocando(false);
     }
 
 
